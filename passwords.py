9174ef86b6dd78ae8773b601a5cb068ea38a555c
mishel254 <sonimichie@gmail.com>
2020-10-09 23:34:47 +0300

    initialazied users class

---
+++ passwords.py
@@ -0,0 +1,13 @@
+class Users
+
+def __init__(self,Fname,Sname,username,Password,email):
+    '''
+    users class 'blueprint'
+    '''
+
+    self.Fname = FirstName
+    self.Sname = SecondName
+    self.username = username
+    self.Password = Password
+    self.email = email
+
