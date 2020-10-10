3f2a6de28bb5758bff1b3890fc31970fb2b9bb9e
mishel254 <sonimichie@gmail.com>
2020-10-11 00:00:41 +0300

    delete method functionality

--- test.py
+++ test.py
@@ -4,43 +4,45 @@ from passwords import User
 
 ## create a subclass 
 class UserTest(unittest.TestCase):
+   
     '''
     Testing class that defines test cases
     '''
-    def tearDown(self):
-       """  '''
-        set up runs befors each test case
+    def setUp(self):
+        '''
+        setUp runs before testcase'
         '''
 
-        self.Users = User('MITCHEL','MUTHONI','NJUGUNAS','YHS67','ABC@CD.EF')
+        self.newUser = User('MITCHEL','MUTHONI','NJUGUNAS','YHS67','ABC@CD.EF')
 
     def test_init(self):
-        self.assertEqual(self.Users.Fname,'MITCHEL')
-        self.assertEqual(self.Users.Sname,'MUTHONI')
-        self.assertEqual(self.Users.username,'NJUGUNAS')
-        self.assertEqual(self.Users.password,'YHS67')
-        self.assertEqual(self.Users.email,'ABC@CD.EF') """
-    User.user_list = []
+        self.assertEqual(self.newUser.Fname,'MITCHEL')
+        self.assertEqual(self.newUser.Sname,'MUTHONI')
+        self.assertEqual(self.newUser.username,'NJUGUNAS')
+        self.assertEqual(self.newUser.password,'YHS67')
+        self.assertEqual(self.newUser.email,'ABC@CD.EF')
+
+    def tearDown(self):
+         User.user_list = []
 
         # save a user
-
     def test_saveUser(self):
-        self.Users.saveUser()
+        self.newUser.saveUser()
         self.assertEqual(len(User.user_list),1)
 
         #save multiple users
     def  test_savemore(self):
-        self.User.saveUser()
+        self.newUser.saveUser()
         test = User('jane','Doe','jadoe','djdj9','jd@jfj.vom')
-        test.save()
+        test.saveUser()
         self.assertEqual(len(User.user_list),2)
 
         # delete user
     def test_delete(self):
-        self.Users.saveUser()
+        self.newUser.saveUser()
         test = User('John','Doe','JD','ddduu773','john@ab.com')
         test.saveUser()
-        self.Users.delete()
+        self.newUser.delete()
         self.assertEqual(len(User.user_list),1)
 
 if __name__ == '__main__':
