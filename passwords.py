ff66d53a285710d35cd5a8e5387baece87fc145e
mishel254 <sonimichie@gmail.com>
2020-10-10 16:14:00 +0300

    delete user object

--- passwords.py
+++ passwords.py
@@ -1,36 +1,24 @@
-import unittest
 class User:
 
 # users list
   user_list  = []
 
-  def save_user(self):
-      user.user_list.append(self)
-
   '''
   initialize a class
   '''
-  def __init__(self,Fname,Sname,username,Password,email):
+  def __init__(self,Fname,Sname,username,password,email):
       '''
       users class 'blueprint'
       '''
       self.Fname = Fname
       self.Sname = Sname
       self.username = username
-      self.Password = Password
+      self.password = password
       self.email = email 
 
-      def test_init(self):
-          self.assertEqual(self.Users.Fname = Fname)
-          self.assertEqual(self.Users.Sname = Sname)
-          self.assertEqual(self.Users.password = password)
-          self.assertEqual(self.Users.username = username)
-          self.assertEqual(self.Users.email = email)
+  def saveUser(self):
+        User.user_list.append(self)
 
-     if __name__ == '__main__':
-         unittest.main()
-
-    def save_user(self):
-        self.Users.save_user()
-        self.assert(len(password.user_list),1)
-
+  def delete(self):
+        User.user_list.remove(self)
+      
