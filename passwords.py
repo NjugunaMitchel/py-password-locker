af22f46ce17a82ac97cb78bbe6656ced062caaf9
mishel254 <sonimichie@gmail.com>
2020-10-10 14:23:56 +0300

    creating a new user

--- passwords.py
+++ passwords.py
@@ -1,13 +1,36 @@
-class Users
+import unittest
+class User:
 
-def __init__(self,Fname,Sname,username,Password,email):
-    '''
-    users class 'blueprint'
-    '''
+# users list
+  user_list  = []
 
-    self.Fname = FirstName
-    self.Sname = SecondName
-    self.username = username
-    self.Password = Password
-    self.email = email
+  def save_user(self):
+      user.user_list.append(self)
+
+  '''
+  initialize a class
+  '''
+  def __init__(self,Fname,Sname,username,Password,email):
+      '''
+      users class 'blueprint'
+      '''
+      self.Fname = Fname
+      self.Sname = Sname
+      self.username = username
+      self.Password = Password
+      self.email = email 
+
+      def test_init(self):
+          self.assertEqual(self.Users.Fname = Fname)
+          self.assertEqual(self.Users.Sname = Sname)
+          self.assertEqual(self.Users.password = password)
+          self.assertEqual(self.Users.username = username)
+          self.assertEqual(self.Users.email = email)
+
+     if __name__ == '__main__':
+         unittest.main()
+
+    def save_user(self):
+        self.Users.save_user()
+        self.assert(len(password.user_list),1)
 
