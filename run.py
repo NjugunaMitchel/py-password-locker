405fa1caf36665f588e29de084d4bf684a8526f3
mishel254 <sonimichie@gmail.com>
2020-10-12 00:38:13 +0300

    create function

---
+++ run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.8
+from passwords import Credentials
+
+def create(fname,lname,user,passwords,email):
+    new_Credentials = Credentials(fname,lname,user,passwords,email
+    return new_Credentials
