559d10b8c79f48cd86835786fe3ab923d24dbd1d
mishel254 <sonimichie@gmail.com>
2020-10-12 01:53:32 +0300

    delete method

--- passwords.py
+++ passwords.py
@@ -1,4 +1,35 @@
-class Credentials:
+class accounts:
+    user_list = []
+
+    def __init__(self,username,password,email):
+        self.username = username
+        self.password = password
+
+    @classmethod
+    def auth_by_email(cls,email):
+        for accounts in cls.user_list:
+            if accounts.email == email:
+                return accounts
+
+    def  account_exists(cls,number):
+        for accounts in cls.user_list:
+            if accounts.email == email:
+                return true
+
+        return false;
+
+    def display_accounts(cls):
+        return cls.user_list
+
+
+
+
+
+
+""" def authn_user(self):
+        details = input('your name') """
+      
+class Credentials(accounts):
 
 # users list
   credentials_list  = []
@@ -23,34 +54,3 @@ class Credentials:
   def delete(self):
         Credentials.credentials_list.remove(self)
 
-class accounts():
-    user_list = []
-
-    def __init__(self,username,password,email):
-        self.username = username
-        self.password = password
-
-@classmethod
-def auth_by_email(cls,email):
-    for accounts in cls.user_list:
-        if accounts.email == email:
-            return accounts
-
-def  account_exists(cls,number):
-    for accounts in cls.user_list:
-        if accounts.email == email:
-            return true
-
-    return false;
-
-def display_accounts(cls):
-    return cls.user_list
-
-
-
-
-
-
-""" def authn_user(self):
-        details = input('your name') """
-      
